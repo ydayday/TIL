b7d99c569244eb0de66549913a1ff2b52c5e83bc
ydayday <tpqmswkd78@gmail.com>
2024-10-14 22:32:24 +0900

    producer 처리량 향상 작업(batch.size, linger.ms, compression)

--- kafka/wikimedia/src/main/java/WikimediaChangesProducer.java
+++ kafka/wikimedia/src/main/java/WikimediaChangesProducer.java
@@ -1,6 +1,7 @@
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
@@ -15,6 +16,12 @@ public class WikimediaChangesProducer {
         properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // 처리량을 늘리기 위한 프로듀서 구성 설정
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(30 * 1024)); // 32KB
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy"); // 압축 타입 설정
+
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
