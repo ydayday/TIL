70d9be36d47c049dd5083b8edb7402273fb528b0
ydayday <tpqmswkd78@gmail.com>
2024-10-23 21:42:35 +0900

    오픈서치 컨슈머 2

--- kafka/consumer-opensearch/src/main/java/OpenSearchConsumer.java
+++ kafka/consumer-opensearch/src/main/java/OpenSearchConsumer.java
@@ -4,16 +4,26 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
 
@@ -48,7 +58,9 @@ public class OpenSearchConsumer {
 
         RestHighLevelClient openSearchClient = createOpenSearchClient();
 
-        try(openSearchClient) {
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+
+        try(openSearchClient; consumer) {
             boolean isIndex = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
 
             if (!isIndex) {
@@ -59,8 +71,48 @@ public class OpenSearchConsumer {
                 log.info("already idx");
             }
 
+            consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+                int count = records.count();
+                log.info("Received " + count + " records");
+
+                for (ConsumerRecord<String, String> record : records) {
+                  // send to record into openSearch
+
+                    try {
+                        IndexRequest indexRequest = new IndexRequest("wikimedia")
+                                .source(record.value(), XContentType.JSON);
+
+                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                        log.info(response.getId());
+
+                    } catch (Exception e) {
+
+                    }
+                }
+
+            }
+
         }
 
 
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        // new group id
+        String groupId = "consumer-opensearch-demo";
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "latest");
+
+
+        return new KafkaConsumer<>(properties);
     }
 }
